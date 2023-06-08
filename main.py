5c1453cb04c866a1b63b87219e83ce059a2ac043
madiscercy <madiscercy@gmail.com>
2023-06-07 21:38:37 -0400

    adding check for query string params

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import json
 
 def lambda_handler(event, context):
-    print("In lambda handler")
-    print(event)
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from ArtAlchemy!')
+    if 'queryStringParameters' in event:
+        query_string_params = event['queryStringParameters']
+        if query_string_params is not None and 'access_token' in query_string_params and 'username' in query_string_params:
+
+            access_token = event['queryStringParameters']['access_token']
+            username = event['queryStringParameters']['username']
+            print(access_token)
+            print(username)
+            return {
+            'statusCode': 200,
+            'body': json.dumps('Hello from ArtAlchemy!')
+            }
+        else:
+            response = {
+            'statusCode': 400,
+            'body': json.dumps('Missing access_token or username')
+            }
+            print(response)
+            return response
+        
+    response = {
+    'statusCode': 400,
+    'body': json.dumps('Missing query string parameters')
     }
+    print(response)
+    return response
+
+
+event = {'queryStringParameters': {'access_token': '1234', 'username': 'wasabiibunni'}}
+lambda_handler(event, None)
