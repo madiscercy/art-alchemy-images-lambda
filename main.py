f067f930551967d00be37e1736a2cf6705b0df86
madiscercy <madiscercy@gmail.com>
2023-06-07 21:06:41 -0400

    print statement

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+    print("In lambda handler")
+    print(event)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from ArtAlchemy!')
