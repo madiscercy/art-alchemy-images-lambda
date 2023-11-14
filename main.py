e163a79e3fa1a3b55df947cea9d9bf9da1d448a9
madiscercy <madiscercy@gmail.com>
2023-11-13 20:33:01 -0500

    update to properly parse body

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ from utils import get_secret
 
 
 def lambda_handler(event, context):
-    # Extracting image data and style from the event
-    base64_image = event['imageData']
-    selected_style = event['style']
-    api_key = get_secret()
+    if 'body' in event:
+        body = json.loads(event['body'])
 
-    # Get image description using ChatGPT
-    prompt = get_image_description(base64_image, api_key, selected_style)
+        base64_image = body.get('imageData')
+        selected_style = body.get('style')
 
-    # Generate a new image using DALL-E with the description and style
-    new_image = generate_image(prompt, api_key)
+        api_key = get_secret()
 
-    # Return the new image as a response (or a URL to the image if stored in S3)
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Origin': '*'  # or your specific origin
-        },
-        'body': json.dumps({'newImage': new_image})
-    }
+        # Get image description using ChatGPT
+        prompt = get_image_description(base64_image, api_key, selected_style)
+
+        # Generate a new image using DALL-E with the description and style
+        new_image = generate_image(prompt, api_key)
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*'  # or your specific origin
+            },
+            'body': json.dumps({'newImage': new_image})
+        }
+    else:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('No payload provided')
+        }
